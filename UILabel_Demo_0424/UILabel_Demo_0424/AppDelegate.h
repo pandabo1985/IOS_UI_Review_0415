b08449fec8aada460a9a8ed1731fcbcb7bc45557
panda <pandabo1985@gmail.com>
2014-04-24 22:46:03 +0800

    UILable Demo just for look api

---
+++ UILabel_Demo_0424/UILabel_Demo_0424/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UILabel_Demo_0424
+//
+//  Created by pan dabo on 14-4-24.
+//  Copyright (c) 2014年 pan dabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UILabel_Demo_0424/UILabel_Demo_0424/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UILabel_Demo_0424
+//
+//  Created by pan dabo on 14-4-24.
+//  Copyright (c) 2014年 pan dabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
