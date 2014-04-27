f72b233e1aa3981cf5f7213c584204b5634bcea7
panda <pandabo1985@gmail.com>
2014-04-27 08:33:15 +0800

    UIButton_Demo API Test

---
+++ UIButton_0427/UIButton_0427/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIButton_0427
+//
+//  Created by pan dabo on 14-4-27.
+//  Copyright (c) 2014年 pan dabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIButton_0427/UIButton_0427/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIButton_0427
+//
+//  Created by pan dabo on 14-4-27.
+//  Copyright (c) 2014年 pan dabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
